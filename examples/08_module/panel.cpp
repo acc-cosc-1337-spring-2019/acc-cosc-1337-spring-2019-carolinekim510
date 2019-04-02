4ff5f918e6f858cb7da4405e7da95d56c15317cf
Caroline Kim <caroline.kim@g.austincc.edu>
2019-04-02 18:47:15 -0500

    Assignment 10

--- examples/08_module/panel.cpp
+++ examples/08_module/panel.cpp
@@ -1,4 +1,12 @@
 #include "panel.h"
+#include <string>
+
+class MyHello
+{
+public:
+	std::string say_hello() { return "Carol"; }
+};
+
 
 /*
 Class Constructor
@@ -8,7 +16,13 @@ Class Constructor
 Panel::Panel(wxWindow* parent) 
 	: wxPanel(parent, -1)
 {
-	
+	auto hello_button = new wxButton(this, -1, wxT("Hello"));	//Panel is the owner
+	hello_button->Bind(wxEVT_BUTTON, &Panel::on_hello, this);
 }
 
+void Panel::on_hello(wxCommandEvent & event)
+{
+	MyHello h;
+	wxLogMessage("Hello World from wxWidgets! " + wxString(h.say_hello()));
+}
 
