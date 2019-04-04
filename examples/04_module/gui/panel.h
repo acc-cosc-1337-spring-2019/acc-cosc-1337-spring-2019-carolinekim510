c95f7679cecf6df13d8269492fe21b7ec92fcff8
Caroline Kim <caroline.kim@g.austincc.edu>
2019-04-04 18:38:50 -0500

    Assignment in class 4/4/2019

--- examples/04_module/gui/panel.h
+++ examples/04_module/gui/panel.h
@@ -1,6 +1,7 @@
 #ifndef PANEL_H
 #define PANEL_H
 #include<wx/wx.h>
+#include "checking_account.h"
 
 
 class Panel : public wxPanel
@@ -9,7 +10,17 @@ public:
 	Panel(wxWindow* parent);
 	
 private:
-
+	void on_submit_button_click(wxCommandEvent & Event);
+	/*
+	Organize components in Sizer
+	*/
+	wxBoxSizer* get_box_sizer();
+	wxStaticText* balance_label;
+	wxTextCtrl* transaction_text;
+	wxButton* submit_button;
+	wxStaticText* balance_text;
+	wxRadioBox* trans_type_radio;
+	std::unique_ptr<BankAccount> account;
 };
 
 #endif // !PANEL_H
