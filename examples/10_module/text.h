eb9b53b34a81de518b863bc647b651ab1f6411e3
Caroline Kim <caroline.kim@g.austincc.edu>
2019-04-16 19:06:05 -0500

    Assignment 12

--- examples/10_module/text.h
+++ examples/10_module/text.h
@@ -4,5 +4,16 @@
 #include "shape.h"
 #include "point.h"
 
+using std::string;
+
+class Text : public Shape
+{
+public:
+	Text(wxDC* dc, string t, Point p) : Shape(dc), text(t), point(p) {}
+	void draw() override;
+private:
+	Point point;
+	string text;
+};
 
 #endif // TEXT_H
