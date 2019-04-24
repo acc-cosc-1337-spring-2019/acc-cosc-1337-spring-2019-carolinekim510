b48ba398b43c2480a618539222ea3360fb12a14e
Caroline Kim <caroline.kim@g.austincc.edu>
2019-04-24 14:29:35 -0500

    4/23/2019 In class

--- examples/11_module/01_ref_pointers/main.cpp
+++ examples/11_module/01_ref_pointers/main.cpp
@@ -1,5 +1,24 @@
+#include "ref_pointers.h"
+#include <iostream>
+
+
 int main() 
 {
-	
+	int number = 10;
+
+	//ref(number);
+	// a pointer can stire the address of a variable
+	int* numberPtr = &number;
+
+	ref(number, numberPtr);
+
+	//display what value? we should see 100
+	//std::cout << number;
+
+	std::cout << "Address of number : " << &number << " value is : " << number << "\n";
+
+	std::cout << "Address of numberPtr: " << &numberPtr << " value stored at address pointed to is: " << numberPtr << 
+		"\nDeference numberPtr: " << *numberPtr;
+
 	return 0;
 }
--- examples/11_module/01_ref_pointers/ref_pointers.cpp
+++ examples/11_module/01_ref_pointers/ref_pointers.cpp
@@ -1,1 +1,8 @@
-//
+#include "ref_pointers.h"
+
+
+void ref(int & number, int* numberPtr)
+{
+	number = 100;
+	*numberPtr = 1000;
+}
