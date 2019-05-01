53486b05c50760fd997f13cb623c1244801bc127
Caroline Kim <caroline.kim@g.austincc.edu>
2019-04-30 19:18:34 -0500

    Inclass 4/30/2019

--- examples/11_module/06_destructors/main.cpp
+++ examples/11_module/06_destructors/main.cpp
@@ -1,6 +1,14 @@
+#include "destructor.h"
+
 int main() 
 {
+	Destructor d(10); 
 
+	Destructor a = d; // create class copy
+	d.set_value(5);
+
+	// Can't work because the memory is already been released and there is no pointing.
+	a.set_ptr_num(100);
 
 	return 0;
 }
--- examples/11_module_test/11_module_test.cpp
+++ examples/11_module_test/11_module_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "ref_pointers.h"
+#include "destructor.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -15,3 +16,33 @@ TEST_CASE("Test funcion reference parameter") {
 	REQUIRE(number == 1000);
 }
 
+TEST_CASE("Test class copy")
+{
+	Destructor a(10);
+	Destructor b = a;
+
+	REQUIRE(a.get_value() == 10);
+	REQUIRE(b.get_value() == 10);
+
+	a.set_value(5);
+	REQUIRE(a.get_value() == 5);
+	REQUIRE(b.get_value() == 10);
+
+}
+
+TEST_CASE("Test class copy dynamic private data")
+{
+	Destructor a(10);
+	Destructor b = a;
+	int* ptr = a.get_ptr_num();
+	int* ptr1 = b.get_ptr_num();
+
+	REQUIRE(*ptr == 10);
+	REQUIRE(*ptr1 == 10);
+
+	a.set_ptr_num(100);
+
+	REQUIRE(*ptr == 100);
+	REQUIRE(*ptr1 == 10);
+
+}
